dd12056112748b75233fa79d2a9c7da0639f691d
Will Rodgers <WillRodgers.e.s@gmail.com>
2020-10-02 12:10:21 -0700

    added files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Doubble Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Spicy Jalapeno Burger');
+INSERT INTO burgers (burger_name) VALUES ('Garden Bistro Burger');
