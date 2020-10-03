a534c593c1f933562ea0e999f430dac78f5c8ebf
Will Rodgers <WillRodgers.e.s@gmail.com>
2020-10-03 15:26:13 -0700

    added querry string builder

--- config/orm.js
+++ config/orm.js
@@ -72,7 +72,13 @@ var orm = {
 	},
 	// An example of objColVals would be {name: panther, sleepy: true}
 	update: function (table, objColVals, condition, cb) {
-	
+	var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
 		console.log(queryString);
 		connection.query(queryString, function (err, result) {
 			if (err) {
